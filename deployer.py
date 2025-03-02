c1d62754591342122e2ad755cf9842e8f594f0af
Jonathan Peterson <jpeterson462@gmail.com>
2025-03-01 21:57:25 -0500

    Added functionality to copy htdocs contents

--- deployer.py
+++ deployer.py
@@ -1,9 +1,13 @@
 import argparse
+import distutils
+import errno
+import os
+import shutil
 import subprocess
 import time
 
-STOP_BITNAMI = ['/opt/bitnami/ctlscript.sh', 'stop', 'apache']
-START_BITNAMI = ['/opt/bitnami/ctlscript.sh', 'start', 'apache']
+STOP_BITNAMI = [] #['/opt/bitnami/ctlscript.sh', 'stop', 'apache']
+START_BITNAMI = [] #['/opt/bitnami/ctlscript.sh', 'start', 'apache']
 
 def process_settings():
     parser = argparse.ArgumentParser(
@@ -11,11 +15,35 @@ def process_settings():
         description='Utility for setting up a bitnami instance'
     )
     parser.add_argument('-c', '--certificate', help='S3 Bucket path for the SSL certificate', required=True)
-    parser.add_argument('-r', '--repo', help='Git repository for the htdocs contents', required=True)
+    parser.add_argument('-r', '--repo', help='Git repository (.git path) for the htdocs contents', required=True)
+    parser.add_argument('-t', '--htdocs', help='Root directory for htdocs', required=True)
     return parser.parse_args()
 
-def execute_command(cmd):
-    return subprocess.run(cmd)
+def execute_command(cmd, cwd=None):
+    if len(cmd) > 0:
+        return subprocess.run(cmd, cwd=cwd)
+
+def create_directory(name):
+    if not os.path.exists(name):
+        os.makedirs(name)
+
+def find_first_subfolder(folder):
+    with os.scandir(folder) as it:
+        for entry in it:
+            if not entry.name.startswith('.') and not entry.is_file():
+                return entry.name
+    return None
+
+def clear_directory(directory):
+    for filename in os.listdir(directory):
+        file_path = os.path.join(directory, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print(f"Failed to delete {file_path}. Reason: {e}")
 
 settings = process_settings()
 
@@ -23,8 +51,18 @@ print('Stopping Bitnami...')
 execute_command(STOP_BITNAMI)
 
 # download latest site code
+print('Downloading htdocs from git...')
+create_directory('tmp')
+clear_directory('tmp')
+execute_command(['git', 'clone', settings.repo], 'tmp')
 
 # copy site code to htdocs
+print('Copying to htdocs folder...')
+repo_folder = find_first_subfolder('tmp')
+print(repo_folder)
+clear_directory(settings.htdocs)
+shutil.copytree('tmp/' + repo_folder, settings.htdocs, dirs_exist_ok=True)
+clear_directory('tmp')
 
 # download SSL certificate
 
